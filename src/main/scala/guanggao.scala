9e85e3c713ce41d886ba9f6aff95e4e5afcbef1d
sx0404 <s1264242822@163.com>
2016-05-26 19:09:56 +0800

    df

--- src/main/scala/guanggao.scala
+++ src/main/scala/guanggao.scala
@@ -9,6 +9,25 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object guanggao {
+
+  val appkey2Product = Map("e2934742f9d3b8ef2b59806a041ab389" -> "ikeyboard",
+    "34c0ab0089e7a42c8b5882e1af3d71f9" -> "lite",
+    "df31bd097babc7cdc13625e8fbc20a1a" -> "hifont",
+    "78472ddd7528bcacc15725a16aeec190" -> "kika",
+    "4e5ab3a6d2140457e0423a28a094b1fd" -> "pro")
+
+  val oid_guanggaowei = Map("kika_home" -> "kika_themerec_facebook_ad",
+    "kika_emoji" -> "kika_keyboard_emoji_ucenter_icon_ads",
+    "kika_settingicon" -> "kika_keyboard_menu_ad",
+    "pro_home" -> "pro_themerec_facebook_ad",
+    "pro_emoji" -> "pro_keyboard_emoji_ucenter_icon_ads",
+    "setting-icon" -> "pro_keyboard_menu_ad",
+    "ikey_tuijianye" -> "ikey_themerec_facebook_ad",
+    "ikey_icon" -> "ikeyboard_keyboard_menu_ad",
+    "lite_kaipinguanggao" -> "lite_splash_Interstitial_ad",
+    "lite_tuijianye" -> "lite_themerec_facebook_ad",
+    "lite_icon" -> "lite_keyboard_menu_ad"
+  )
   def main(args: Array[String]) = {
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
@@ -34,28 +53,30 @@ object guanggao {
         "driver" -> "com.mysql.jdbc.Driver"
       )
     ).load()
-    val sql_str = "SELECT day,SUM(clicks),SUM(impressions),SUM(installs) FROM `ad` where ad_id = 2854005 GROUP BY `day`"
+    val sql_str = "SELECT day,oid,app_key,SUM(clicks),SUM(impressions),SUM(installs) FROM `ad` where app_key=\"e2934742f9d3b8ef2b59806a041ab389\" or app_key = \"34c0ab0089e7a42c8b5882e1af3d71f9\" or app_key=\"78472ddd7528bcacc15725a16aeec190\" or app_key=\"4e5ab3a6d2140457e0423a28a094b1fd\" GROUP BY `day`,oid"
 
     jdbcDF.registerTempTable("ad")
     val jdbc = jdbcDF
       .sqlContext.sql(sql_str)
       .map{x =>
-        (x(0).toString, x(1).toString, x(2).toString, x(3).toString)
+        (x(0).toString, x(1).toString, x(2).toString, x(3).toString,x(4).toString,x(5).toString)
       }
       .collect()
     jdbc
   }
 
-  def write_csv(result:Array[(String,String,String,String)]): Unit ={
+  def write_csv(result:Array[(String,String,String,String,String,String)]): Unit ={
     val calendar =  Calendar.getInstance()
     val date = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime())
     val path = "mail_jianglong/" + date + ".csv"
     val wr = new OutputStreamWriter(new FileOutputStream(new File(path)), "UTF8")
     val writers = new BufferedWriter(wr)
-    writers.write("day" + "," + "sum(clicks)" + "," + "sum(impretions)" + "," + "sum(installs)" + "\r\n")
+    writers.write("day" + "," + "guangaowei" + "," +"app_name" + "," +"sum(clicks)" + "," + "sum(impretions)" + "," + "sum(installs)" + "\r\n")
     for (item <- result) {
       println("sx" + item)
-      writers.write(item._1 + "," + item._2 + "," + item._3 + "," + item._4 + "\r\n");
+      val guanggaowei = oid_guanggaowei(item._2)
+      val app_name = appkey2Product(item._3)
+      writers.write(item._1 + "," + guanggaowei + "," + app_name + "," + item._4 + "," + item._5 +"," + item._6 +"\r\n");
     }
     writers.close()
   }
